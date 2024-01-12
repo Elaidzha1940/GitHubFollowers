9f5113cfe672fdc1d7c26f70349cbc46297b400a
Elaidzha1940 <eli080293@gmail.com>
2024-01-12 12:11:31 +0300

    Add GFButton

---
+++ GitHubFollowers/GFButton.swift
@@ -0,0 +1,38 @@
+//  /*
+//
+//  Project: GitHubFollowers
+//  File: GFButton.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 12.01.2024
+//
+//  */
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        //Custom code
+        configure()
+    }
+    
+    // I don't use any Storyboard
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 15
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
