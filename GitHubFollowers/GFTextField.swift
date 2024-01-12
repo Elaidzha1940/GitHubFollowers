d4035e10488d3fc9c4f35dfc11d282a5c4ee3ba4
Elaidzha1940 <eli080293@gmail.com>
2024-01-12 12:32:48 +0300

    Add GFTextField

---
+++ GitHubFollowers/GFTextField.swift
@@ -0,0 +1,43 @@
+//  /*
+//
+//  Project: GitHubFollowers
+//  File: GFTextField.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 12.01.2024
+//
+//  */
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    // I don't use any Storyboard
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 15
+        layer.borderWidth = 3
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Enter a username"
+    }
+}
