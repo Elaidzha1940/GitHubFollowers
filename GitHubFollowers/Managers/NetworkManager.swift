0196ca4c1d79534684d2670a2b92edcb762aa166
Elaidzha1940 <eli080293@gmail.com>
2024-01-14 17:42:45 +0300

    Update NetworkManager

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -25,7 +25,29 @@ class NetworkManager {
         }
         
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            <#code#>
+            if let _ = error {
+                completed(nil, "Unable to complete your request. Plese cheack your internet connection.")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(nil, "Invalid response from the server. Plese try again.")
+                return
+            }
+            
+            guard let data = data else {
+                completed(nil, "The data received from the server was invalid. Plese try again.")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let followers = try decoder.decode([Follower].self, from: data)
+                completed(followers, nil)
+            } catch {
+                completed(nil, "The data received from the server was invalid. Plese try again.")
+            }
         }
         task.resume()
     }
