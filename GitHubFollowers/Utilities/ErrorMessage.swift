e80443aa8199dbfa80ff27e8d7ef4e70392d9e17
Elaidzha1940 <eli080293@gmail.com>
2024-01-15 16:17:32 +0300

    Added Utilities / ErrorMessage

---
+++ GitHubFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//  /*
+//
+//  Project: GitHubFollowers
+//  File: ErrorMessage.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 14.01.2024
+//
+//  */
+
+import Foundation
+
+enum ErrorMessage: String {
+    
+    case invalidUsername = "Invalid response from the server. Plese try again."
+}
