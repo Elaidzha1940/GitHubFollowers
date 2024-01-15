d2215173f575773477393359b39c48ce52fa8278
Elaidzha1940 <eli080293@gmail.com>
2024-01-15 18:33:34 +0300

    Addedn GFAvatarImageView + Cells

---
+++ GitHubFollowers/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,17 @@
+//  /*
+//
+//  Project: GitHubFollowers
+//  File: FollowerCell.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 15.01.2024
+//
+//  */
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    static let userID = "FollowerCell"
+    
+    let avatarImageView = UIImageView()
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+}
---
+++ GitHubFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,32 @@
+//  /*
+//
+//  Projcet: GitHubFollowers
+//  File: GFAvatarImageView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 15.01.2024
+//
+//  */
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        conficure()
+    }
+    
+    // I don't use any Storyboard
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func conficure() {
+        layer.cornerRadius = 15
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
