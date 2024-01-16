942102fc2ed3ba692230a5303f397456e5401633
Elaidzha1940 <eli080293@gmail.com>
2024-01-16 16:34:04 +0300

    Update GitHubFollowers

--- GitHubFollowers/Custom Views/GFAvatarImageView.swift
+++ GitHubFollowers/Custom Views/GFAvatarImageView.swift
@@ -25,7 +25,7 @@ class GFAvatarImageView: UIImageView {
     }
     
     private func configure() {
-        layer.cornerRadius = 10
+        layer.cornerRadius = 15
         clipsToBounds = true
         image = placeholderImage
         translatesAutoresizingMaskIntoConstraints = false
