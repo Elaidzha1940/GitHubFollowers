7af5e3b643c5e1ccf6b6dae12536edb3632dd7d6
Elaidzha1940 <eli080293@gmail.com>
2024-01-17 22:02:53 +0300

    Added GFEmptyState

--- GitHubFollowers/Custom Views/Views/GFEmptyState.swift
+++ GitHubFollowers/Custom Views/Views/GFEmptyState.swift
@@ -1,20 +1,54 @@
+//  /*
 //
-//  GFEmptyState.swift
-//  GitHubFollowers
+//  Project: GitHubFollowers
+//  File: GFEmptyState.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 17.01.2024
 //
-//  Created by Elaidzha Shchukin on 17.01.2024.
-//
+//  */
 
 import UIKit
 
 class GFEmptyState: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    let messageLabel = GFTitleLabel(textAlignment: .center, fontSize: 28)
+    let logoImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(message: String) {
+        super.init(frame: .zero)
+        messageLabel.text = message
+        configure()
+        
+    }
+    
+    private func configure() {
+        addSubview(messageLabel)
+        addSubview(logoImageView)
+        
+        messageLabel.numberOfLines = 3
+        messageLabel.textColor = .secondaryLabel
+        
+        logoImageView.image = UIImage(named: "empty-state-logo")
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -150),
+            messageLabel.leadingAnchor.constraint(equalTo: self.leftAnchor, constant: 40),
+            messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
+            messageLabel.heightAnchor.constraint(equalToConstant: 200),
+            
+            logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
+            logoImageView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
+            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 200),
+            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 140)
+        ])
+    }
 }
