ac19d453596fc4e3608e6bda08de22e0b8375a70
Elaidzha1940 <eli080293@gmail.com>
2024-01-17 19:02:47 +0300

    Update Custom Views

---
+++ GitHubFollowers/Custom Views/Views/GFEmptyState.swift
@@ -0,0 +1,20 @@
+//
+//  GFEmptyState.swift
+//  GitHubFollowers
+//
+//  Created by Elaidzha Shchukin on 17.01.2024.
+//
+
+import UIKit
+
+class GFEmptyState: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
