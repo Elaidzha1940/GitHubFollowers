260528dec23a24d3fe783c7d9d632062d6033990
Elaidzha1940 <eli080293@gmail.com>
2024-01-19 01:14:39 +0300

    Added UserInfoVc

---
+++ GitHubFollowers/Screens/UserInfoVc.swift
@@ -0,0 +1,18 @@
+//  /*
+//
+//  Project: GitHubFollowers
+//  File: UserInfoVc.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 19.01.2024
+//
+//  */
+
+import UIKit
+
+class UserInfoVc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
