8d5b661f7f1a37999c016697411e808462905c3a
Elaidzha1940 <eli080293@gmail.com>
2024-01-20 13:40:58 +0300

    Added GFItemInfoView

---
+++ GitHubFollowers/Custom Views/Views/GFItemInfoView.swift
@@ -0,0 +1,53 @@
+//  /*
+//
+//  Project: GitHubFollowers
+//  File: GFItemInfoView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 20.01.2024
+//
+//  */
+
+import UIKit
+
+class GFItemInfoView: UIView {
+    
+    let symbolImageView = UIImageView()
+    let titleLabel = GFTitleLabel(textAlignment: .left, fontSize: 15)
+    let countLabel = GFTitleLabel(textAlignment: .center, fontSize: 15)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        addSubview(symbolImageView)
+        addSubview(titleLabel)
+        addSubview(countLabel)
+       
+        symbolImageView.translatesAutoresizingMaskIntoConstraints = false
+        symbolImageView.contentMode = .scaleAspectFill
+        symbolImageView.tintColor = .label
+        
+        NSLayoutConstraint.activate([
+            symbolImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            symbolImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            symbolImageView.widthAnchor.constraint(equalToConstant: 20),
+            symbolImageView.heightAnchor.constraint(equalToConstant: 20),
+            
+            titleLabel.centerYAnchor.constraint(equalTo: symbolImageView.centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: symbolImageView.trailingAnchor, constant: 13),
+            titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            titleLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            countLabel.topAnchor.constraint(equalTo: symbolImageView.bottomAnchor, constant: 5),
+            countLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            countLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            countLabel.heightAnchor.constraint(equalToConstant: 20),
+        ])
+    }
+}
