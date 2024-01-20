e2d8a69662c576cafafee6b950a6e5dd64cb2c80
Elaidzha1940 <eli080293@gmail.com>
2024-01-20 20:48:06 +0300

    Added GFRepoItemVC

---
+++ GitHubFollowers/Custom Views/ViewControlers/GFRepoItemVC.swift
@@ -0,0 +1,25 @@
+//  /*
+//
+//  Projcet: GitHubFollowers
+//  File: GFRepoItemVC.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 20.01.2024
+//
+//  */
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPink, title: "GitHub Profile")
+    }
+}
+
