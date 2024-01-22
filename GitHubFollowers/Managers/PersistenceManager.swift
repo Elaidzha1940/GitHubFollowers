d344ab4df34d0b3aac1a3951d97c20ec6defb392
Elaidzha1940 <eli080293@gmail.com>
2024-01-22 12:16:53 +0300

    Update GFError

--- GitHubFollowers/Managers/PersistenceManager.swift
+++ GitHubFollowers/Managers/PersistenceManager.swift
@@ -9,10 +9,65 @@
 
 import Foundation
 
+enum PersistenceActionType {
+    case add, remove
+}
+
 enum PersistenceManager {
     static private let defaults = UserDefaults.standard
     
-    static func retrieveFavorites(compledet: @escaping(Result<[Follower], GFError>) -> ()) {
+    enum Keys {
+        static let favorites = "favorites"
+    }
+    
+    static func updateWith(favorite: Follower, actionType: PersistenceActionType, completed: @escaping (GFError?) -> ()) {
+        retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                var retrieveFavorites = favorites
+                switch actionType {
+                case .add:
+                    guard !retrieveFavorites.contains(favorite) else {
+                        completed(.invalidData)
+                    }
+                case .remove:
+                    break
+                }
+                
+                
+            case .failure(let  error):
+                completed(error)
+                
+            }
+        }
+    }
+    
+    static func retrieveFavorites(completed: @escaping(Result<[Follower], GFError>) -> ()) {
+        guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
+            completed(.success([]))
+            return
+        }
         
+        do {
+            let decoder = JSONDecoder()
+            let favorites = try decoder.decode([Follower].self, from: favoritesData)
+            completed(.success(favorites))
+        } catch {
+            completed(.failure(.unableToFavorite))
+        }
+    }
+    
+    static func save(favorites: [Follower]) -> GFError? {
+        
+        do {
+            let encoder = JSONEncoder()
+            let encoderFavorites = try encoder.encode(favorites)
+            defaults.setValue(encoderFavorites, forKey: Keys.favorites)
+            return nil
+        } catch {
+            return .unableToFavorite
+        }
+        
+        return nil
     }
 }
--- GitHubFollowers/Utilities/GFError.swift
+++ GitHubFollowers/Utilities/GFError.swift
@@ -11,8 +11,10 @@ import Foundation
 
 enum GFError: String, Error {
     
-    case invalidUsername = "This username created an invalid request. Plese try again."
+    case invalidUsername = "This username created an invalid request. Please try again."
     case unableToComplete  = "Unable to complete your request. Plese cheack your internet connection."
     case invalidResponse = "Invalid response from the server. Plese try again."
-    case invalidData = "The data received from the server was invalid. Plese try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+    case unableToFavorite = "There were an error favoriting this user. Please try again."
+    case alreadyInFavorites = "You've already favorited this user. You must Really like them."
 }
