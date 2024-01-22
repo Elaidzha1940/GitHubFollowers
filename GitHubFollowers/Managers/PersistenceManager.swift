e90c487b0cd01fd4ba4864c9acee55951c07bead
Elaidzha1940 <eli080293@gmail.com>
2024-01-22 11:19:13 +0300

    Added PersistenceManager

---
+++ GitHubFollowers/Managers/PersistenceManager.swift
@@ -0,0 +1,18 @@
+//  /*
+//
+//  Projcet: GitHubFollowers
+//  File: PersistenceManager.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 22.01.2024
+//
+//  */
+
+import Foundation
+
+enum PersistenceManager {
+    static private let defaults = UserDefaults.standard
+    
+    static func retrieveFavorites(compledet: @escaping(Result<[Follower], GFError>) -> ()) {
+        
+    }
+}
