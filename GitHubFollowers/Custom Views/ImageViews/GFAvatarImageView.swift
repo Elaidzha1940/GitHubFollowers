165bba1dbc8f9b5a5251111f9744b38b725ab684
Elaidzha1940 <eli080293@gmail.com>
2024-01-24 22:49:08 +0300

    Added GFDataLoadingVC

--- GitHubFollowers/Custom Views/ImageViews/GFAvatarImageView.swift
+++ GitHubFollowers/Custom Views/ImageViews/GFAvatarImageView.swift
@@ -33,31 +33,4 @@ class GFAvatarImageView: UIImageView {
         widthAnchor.constraint(equalToConstant: 75).isActive = true
         heightAnchor.constraint(equalToConstant: 75).isActive = true
     }
-    
-    func downloadImage(from urlString: String) {
-        
-        let cacheKey = NSString(string: urlString)
-        
-        if let image = cache.object(forKey: cacheKey) {
-            self.image = image
-        }
-        
-        guard let url = URL(string: urlString) else { return }
-        
-        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
-            guard let self = self else { return }
-            
-            if error != nil { return }
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
-            guard let data = data else { return }
-            
-            guard let image = UIImage(data: data) else { return }
-            self.cache.setObject(image, forKey: cacheKey)
-            
-            DispatchQueue.main.async {
-                self.image = image
-            }
-        }
-        task.resume()
-    }
 }
