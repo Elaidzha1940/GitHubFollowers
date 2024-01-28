c105586ff94fe25b135ecad7aa04157a6c269c33
Elaidzha1940 <eli080293@gmail.com>
2024-01-29 01:30:43 +0300

    Update GFAvatarImageView

--- GitHubFollowers/Custom Views/ImageViews/GFAvatarImageView.swift
+++ GitHubFollowers/Custom Views/ImageViews/GFAvatarImageView.swift
@@ -34,11 +34,6 @@ class GFAvatarImageView: UIImageView {
     }
     
     func downloadImage(from url: String) {
-        NetworkManager.shared.downloadImage(from: url) { [weak self] image in
-            guard let self = self else { return }
-            DispatchQueue.main.async {
-                self.image = image
-            }
-        }
+        Task { image = await NetworkManager.shared.downloadImage(from: url) ?? placeholderImage }
     }
 }
